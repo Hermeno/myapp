b310d138f4e02e886606097a043cdbcae358dbee
HerminioUnisced <hmacamo@estudantes.unisced.edu.mz>
2024-09-26 10:46:52 +0200

    version first

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,9 +4,16 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+// import { ParallaxScrollView } from 'react-native-parallax-scroll-view'; // Importando o componente de paralaxe
+// import { ThemedView, ThemedText } from './path/to/your/themed/components'; // Componentes personalizados
+// import HelloWave from './HelloWave'; // Importando um componente HelloWave
+
+
+
 
 export default function HomeScreen() {
-  return (
+
+  return(
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
@@ -15,40 +22,34 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+
+
+
+
+    <ThemedView style={styles.titleContainer}>
+      <ThemedText type="title">Ola Mundo</ThemedText>
+    </ThemedView>
+
+
+
+
+
+
     </ParallaxScrollView>
-  );
+  )
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   titleContainer: {
@@ -61,9 +62,9 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
+    height: 40,
     width: 290,
-    bottom: 0,
+    top: 0,
     left: 0,
     position: 'absolute',
   },
--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
-    height: 250,
+    height: 80,
     overflow: 'hidden',
   },
   content: {
